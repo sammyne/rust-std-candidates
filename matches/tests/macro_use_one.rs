e6758b09d49d928b2a035295174cbd52f17fe321
sammyne <lixiangmin01@baidu.com>
2020-04-12 05:02:41 +0000

    feature: upgrade to rust-2018 and format the code

--- matches/tests/macro_use_one.rs
+++ matches/tests/macro_use_one.rs
@@ -1,5 +1,6 @@
 // https://github.com/SimonSapin/rust-std-candidates/issues/12
-#[macro_use(matches)] extern crate matches;
+#[macro_use(matches)]
+extern crate matches;
 
 #[test]
 fn matches_works() {
